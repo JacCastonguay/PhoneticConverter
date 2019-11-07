2a46f1e77180bdf7621b53743a95906f2894b702
JacCastonguay <32004393+JaxCastonguay@users.noreply.github.com>
2019-11-06 23:13:47 -0600

    Adds tests

---
+++ PhoneticTranslatorTests/TranslationTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhoneticTranslator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneticTranslator.Tests
+{
+    [TestClass()]
+    public class TranslationTests
+    {
+        [TestMethod()]
+        public void LetterSubstitueUTest()
+        {
+            //Should only compare specific chars to avoid errors from other letters
+            string awtor = Translation.LetterSubstitue("autor");
+            Assert.AreEqual("awtor"[1], awtor[1]);
+
+            string brujo = Translation.LetterSubstitue("brujo");
+            Assert.AreEqual("brujo"[2], brujo[2]);
+        }
+    }
+}
